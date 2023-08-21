9fd466a35556a31067ba511a9217183d7abce225
acse-fb3ff74a <xuefei.mi22@imperial.ac.uk>
2023-08-21 06:47:39 +0100

    mpi_test

--- examples/Stochastic_opt/small_try.py
+++ examples/Stochastic_opt/small_try.py
@@ -1,62 +1,19 @@
-# import matplotlib.pyplot as plt
+from mpi4py import MPI
 
+# Initialize MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
 
-# x_initial = [0, 667, 1334, 2000, 667, 1334, 0, 667, 1334, 2000] # 3*D, 6 * D, 6 * D,
-# y_initial = [0.0, 0.0, 0.0, 0.0, 1000.0, 1000.0, 2000.0, 2000.0, 2000.0, 2000.0] # 4 * D, 0, 4 * D,
-# x_opt = [100.0]
-# y_opt = [100.0]
+# Generate data on each process
+data = rank + 1
 
-# fig, ax = plt.subplots(1, 1, figsize=(9, 6))
+# Perform the reduction (sum) across all processes
+total_sum = comm.reduce(data, op=MPI.SUM, root=0)
 
-# fontsize = 12
-# ax.plot(x_initial, y_initial, "ob")
-# ax.plot(x_opt, y_opt, "or")
+# Print results
+if rank == 0:
+    print("Total sum:", total_sum)
 
-# ax.set_xlabel("x (m)", fontsize=fontsize)
-# ax.set_ylabel("y (m)", fontsize=fontsize)
-# ax.axis("equal")
-# ax.grid()
-# ax.tick_params(which="both", labelsize=fontsize)
-
-# # # Create legend for the first group of labels
-# legend1 = ax.legend(
-#     ["Old locations", "New locations"],
-#     # loc="upper right",
-#     bbox_to_anchor=(0.5, 1.01),
-#     ncol=2,
-#     fontsize=fontsize,
-# )
-
-# # Add the first legend to the plot
-# ax.add_artist(legend1)
-# length = 2000.0
-# boundaries = [(0.0, 0.0), (0.0, length), (length, length), (length, 0.0), (0.0, 0.0)]
-# verts = boundaries
-# for i in range(len(verts)):
-#     if i == len(verts) - 1:
-#         plt.plot([verts[i][0], verts[0][0]], [verts[i][1], verts[0][1]], "b")
-#     else:
-#         plt.plot(
-#             [verts[i][0], verts[i + 1][0]], [verts[i][1], verts[i + 1][1]], "b"
-#         )
-
-# # Create legend for the second group of labels
-# # plot_boundary(ax, ["fixed", "flexible"], ["--", ":"])
-# boundary_styles = ["--", ":"]
-# boundary_names = ["fixed", "flexible"]
-# boundary_list = [[(0.0, 0.0), (0.0, 100), (100, 100), (100, 0.0), (0.0, 0.0)],\
-#                     [(0.0, 0.0), (0.0, 200), (200, 200), (200, 0.0), (0.0, 0.0)]]
-# for i in range(len(boundary_list)):
-#     x_coords, y_coords = zip(*boundary_list[i])
-#     ax.plot(x_coords, y_coords, linestyle=boundary_styles[i], label=boundary_names[i], color='k')
-#     legend2 = ax.legend()
-
-# ax.add_artist(legend2)
-
-
-# # Save the plot
-# # plt.savefig(path)
-# plt.show()
-import numpy as np
-variance_wd_range = np.linspace(15, 90, 6)#4
-print(variance_wd_range)
+# Finalize MPI
+MPI.Finalize()
